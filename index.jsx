1823b6f13abc786deed61731c9f41306fe340051
Pavle Ristic <pavleristic1@gmail.com>
2025-12-01 23:05:08 +0100

    Updating import

--- index.jsx
+++ index.jsx
@@ -1,5 +1,5 @@
 
-import { createRoot } from react-dom/clients
+import { createRoot } from "react-dom/clients"
 
 const root = createRoot(document.getElementById("root"))
 root.render(<h1>Hello, world</h1>)
