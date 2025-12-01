29494d27c394c0817d16f2221f069bf7cb11fbc2
Pavle Ristic <pavleristic1@gmail.com>
2025-12-01 23:34:22 +0100

    Updating jsx

--- index.jsx
+++ index.jsx
@@ -1,4 +1,5 @@
 import { createList } from "react-dom/clients"
+import { createRoot } from "react"
 
 createList(document.querySelector("#root")).render(
     <ul>
@@ -6,4 +7,10 @@ createList(document.querySelector("#root")).render(
         <li>test2</li>
         <li>test3</li>
     </ul>
+)
+
+const reactElement = <h1>Hello from jsx</h1>
+
+root.render(
+    reactElement
 )
