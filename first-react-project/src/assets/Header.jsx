a03d61c9b88c6a0c5034250877967679637f447c
Pavle Ristic <pavleristic1@gmail.com>
2025-12-03 22:20:00 +0100

    Updating header.jsx

--- first-react-project/src/assets/Header.jsx
+++ first-react-project/src/assets/Header.jsx
@@ -2,6 +2,10 @@ function HeaderTest() {
   return (
     <div>
         <h1>React test</h1>
+        <p>This is descriptions</p>
+    </div>,
+    <div> 
+      <h2>React test header 2</h2>
     </div>
   )
 }
